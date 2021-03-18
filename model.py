96e9d753ffdfc958637e721677e421949774d15c
ananth-vikram <51108730+ananth-vikram@users.noreply.github.com>
2021-03-18 09:49:54 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,28 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+
+dataset = pd.read_csv('ipl_auction_valuation.csv')
+
+
+X = dataset[['matches_played','wickets','runs','catches']]
+
+y = dataset[['auction_valuation']]
+
+#Splitting Training and Test Set
+#Since we have a very small dataset, we will train our model with all availabe data.
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+
+#Fitting model with trainig data
+regressor.fit(X, y)
+
+# Saving model to disk
+pickle.dump(regressor, open('model.pkl','wb'))
+
+# Loading model to compare the results
+model = pickle.load(open('model.pkl','rb'))
+print(model.predict([[200, 90, 60,12]]))
